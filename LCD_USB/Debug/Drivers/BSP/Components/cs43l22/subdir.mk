bc683cb215e8acd0dedb7efb7f3d6601c1e72de1
Bakeu127 <phann4050@gmail.com>
2024-01-25 17:40:56 +0700

    SG

---
+++ LCD_USB/Debug/Drivers/BSP/Components/cs43l22/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/Components/cs43l22/cs43l22.c 
+
+OBJS += \
+./Drivers/BSP/Components/cs43l22/cs43l22.o 
+
+C_DEPS += \
+./Drivers/BSP/Components/cs43l22/cs43l22.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/Components/cs43l22/%.o: ../Drivers/BSP/Components/cs43l22/%.c Drivers/BSP/Components/cs43l22/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"C:/D/ThuchanhNhungReal/LAB_LCD_USB/LCD_USB/Drivers/BSP/Components" -I"C:/D/ThuchanhNhungReal/LAB_LCD_USB/LCD_USB/Drivers/BSP/STM32F429I-Discovery" -I"C:/Users/ASUS/STM32Cube/Repository/STM32Cube_FW_F4_V1.27.1/Drivers/CMSIS/BSP" -I"C:/Users/ASUS/STM32Cube/Repository/STM32Cube_FW_F4_V1.27.1/Drivers/BSP" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
---
+++ LCD_USB/Debug/Drivers/BSP/STM32F429I-Discovery/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery.c \
+../Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_eeprom.c \
+../Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_gyroscope.c \
+../Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_io.c \
+../Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_lcd.c \
+../Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_sdram.c \
+../Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_ts.c 
+
+OBJS += \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery.o \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_eeprom.o \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_gyroscope.o \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_io.o \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_lcd.o \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_sdram.o \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_ts.o 
+
+C_DEPS += \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery.d \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_eeprom.d \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_gyroscope.d \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_io.d \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_lcd.d \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_sdram.d \
+./Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_ts.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/STM32F429I-Discovery/%.o: ../Drivers/BSP/STM32F429I-Discovery/%.c Drivers/BSP/STM32F429I-Discovery/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"C:/D/ThuchanhNhungReal/LAB_LCD_USB/LCD_USB/Drivers/BSP/Components" -I"C:/D/ThuchanhNhungReal/LAB_LCD_USB/LCD_USB/Drivers/BSP/STM32F429I-Discovery" -I"C:/Users/ASUS/STM32Cube/Repository/STM32Cube_FW_F4_V1.27.1/Drivers/CMSIS/BSP" -I"C:/Users/ASUS/STM32Cube/Repository/STM32Cube_FW_F4_V1.27.1/Drivers/BSP" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
